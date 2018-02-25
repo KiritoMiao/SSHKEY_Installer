1de2afc1474f9e52bce577c9fcf0996ef2e60df9
ILLKX <ILLKX@users.noreply.github.com>
2018-02-25 23:17:51 +0800

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,7 +1,8 @@
 #/bin/sh
-apt-get update
+apt-get update -y
 apt-get install curl -y
-yum update
+yum clean all
+yum make cache
 yum install curl -y
 echo '============================
       SSH Key Installer
